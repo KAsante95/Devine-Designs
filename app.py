c9d5edfb1559d184e65d75c9ea4f741c5c83842a
Kwame Asante <kasante.asante@gmail.com>
2016-11-23 12:52:04 -0500

    navigation

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 	return render_template('index.html')
+@app.route("/index.html")
+def front():
+	return render_template('index.html')
+
+@app.route("/generic.html")
+def about():
+	return render_template('generic.html')
+
+@app.route("/elements.html")
+def elements():
+	return render_template('elements.html')
 
 if __name__ == "__main__":
 	app.run() 
